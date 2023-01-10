20988228b363612db3ce71168da1f85ae3353c06
choiseongjun <csj2702@naver.com>
2023-01-10 16:45:55 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        src={"/img/vs.jpeg"}
+        style={{ width: 800, height: 1023 }}
+        alt="BigCo Inc.  "
+      />
+      <div>
+        <input type="text" className="username" placeholder="이름"></input>
+      </div>
+      <div className="phone">
+        <input type="text" className="username" placeholder="이름"></input> -
+        {"\t"}
+        <input type="text" className="username" placeholder="연락처"></input> -
+        {"\t"}
+        <input type="text" className="username" placeholder="연락처"></input>
+      </div>
+      <div className="btnArea">
+        <button>신청하기 </button>
+      </div>
     </div>
   );
 }
