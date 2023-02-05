1bb5ffed6b22591d457e120a218e6ea61c0bb140
choiseongjun <csj2702@naver.com>
2023-02-05 22:15:46 +0900

    1.이미지 변경

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <div className="sub">
         <div className="App">
           <img
-            src={"/img/vs.jpeg"}
+            src={"/img/logo.png"}
             style={{ width: 800, height: 1023, marginLeft: 20 }}
             alt="BigCo Inc.  "
           />
@@ -29,11 +29,10 @@ function App() {
           </div>
         </div>
         <div className="footerInfo">
-          <div>상호명:(주)SB Global 대표자: 여태민 등록번호: 370-88-02297</div>
+          <div>상호명:(주)SB Global 대표자: 허한솔 </div>
           <div>
             소재지: 고양시 대자동 업종:증권정보교육 프로그램개발 및 판매
           </div>
-          <div>TEL: 1577-2650</div>
         </div>
       </div>
     </div>
