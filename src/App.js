052ba3f7b3c1eaca2037df0095a3615aef431608
choiseongjun <csj2702@naver.com>
2023-03-08 17:40:34 +0900

    css test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./Main";
 import Main2 from "./Main2";
+import Main3 from "./Main3";
 
 const App = () => {
   return (
@@ -10,6 +11,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Main />}></Route>
           <Route path="/news" element={<Main2 />}></Route>
+          <Route path="/test" element={<Main3 />}></Route>
+
           {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
         </Routes>
       </BrowserRouter>
---
+++ src/Main3.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Main3.css";
+const Main3 = () => {
+  return (
+    <div className="main">
+      <div className="rect">
+        <div className="grid">
+          <div className="temp"></div>
+          <div className="temp"></div>
+        </div>
+        {/* <div className="rectcon">
+          <div className="bigrect2"></div>
+          <div className="bigrect3"></div>
+        </div>
+        <div className="rectcon">
+          <div className="bigrect2"></div>
+          <div className="bigrect3"></div>
+        </div> */}
+        {/* <div className="rect1"></div>
+        <div className="rect1"></div>
+
+        <div className="rect1"></div>
+
+        <div className="rect1"></div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Main3;
