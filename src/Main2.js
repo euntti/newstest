4f0056076dd68922127f79774d9d24186ce0d898
응띠쀼 <chldmsdud827@naver.com>
2023-05-09 11:20:42 +0900

    버틍

--- src/Main2.js
+++ src/Main2.js
@@ -114,6 +114,46 @@ function Main2() {
       <div>
       <div className="nav">
         <div>SB그룹</div>
+        <p class="box banner">
+                <img src="/img/kakao.png"></img>
+              {/* {isMobile && <img src="/static/images/mNotice.jpeg"></img>} */}
+              <a
+                href="#"
+                onClick={() => {
+                  window.open("https://open.kakao.com/me/shon04Se", "_blank");
+                }}
+              >
+                {isDesktop ? (
+                  <button
+                    style={{
+                      position: "absolute",
+                      bottom: "2%",
+                      right: "36%",
+                      backgroundColor: "#f25700",
+                      color: "yellow",
+                      fontFamily: "GmarketSansMedium",
+                      padding: 15,
+                    }}
+                  >
+                    1:1 카톡 상담하기{" "}
+                  </button>
+                ) : (
+                  <></>
+                  // <button
+                  //   style={{
+                  //     position: "absolute",
+                  //     bottom: "2%",
+                  //     right: "20%",
+                  //     backgroundColor: "#000",
+                  //     color: "orange",
+                  //     padding: 5,
+                  //   }}
+                  // >
+                  //   1:1 카톡 상담가능{" "}
+                  // </button>
+                )}
+              </a>
+            </div>
       </div>
       <div className="box_container">
         <div className="box">
