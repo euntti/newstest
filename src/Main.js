0ba1bbcfba1b29b9fa314280cf91db00a3f4add6
응띠쀼 <chldmsdud827@naver.com>
2023-05-10 18:09:28 +0900

    수정

--- src/Main.js
+++ src/Main.js
@@ -385,7 +385,7 @@ function App() {
                 {isBrowser ? (
                   <img src={"/img/btn.gif"}></img>
                 ) : (
-                  <img style={{ width:"375px" height:"45px"  }} src={"/img/btn.gif"}></img>
+                  <img style={{ width:"375px"  }} src={"/img/btn.gif"}></img>
                 )}
               </button>
             </div>
