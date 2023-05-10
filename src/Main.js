d7907bdd8a8b2e10705dea1962c969fcaf886efe
응띠쀼 <chldmsdud827@naver.com>
2023-05-10 15:27:52 +0900

    가운데정렬

--- src/Main.js
+++ src/Main.js
@@ -80,7 +80,7 @@ function App() {
     const telegramApi = new TelegramApi(TELEGRAM_TOKEN);
     telegramApi.sendMessage(
       TELEGRAM_CHAT_ID,
-      `휴대폰 번호 ${phone1} ${userName}님이 신청하였습니다. `
+      `휴대폰 번호 ${phone1} ${e}님이 신청하였습니다. `
     );
     alert("신청되었습니다.");
   };
@@ -307,7 +307,7 @@ function App() {
             )}
           </div> */}
           <div>
-          <div className="namephone">
+          <div className="nameArea">
             {/* <div className="nameArea">
               <input
                 type="text"
