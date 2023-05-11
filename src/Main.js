869d7d639169c1e19bb5f38d0fde1abba4e1beea
응띠쀼 <chldmsdud827@naver.com>
2023-05-11 16:47:17 +0900

    수익률인증

--- src/Main.js
+++ src/Main.js
@@ -391,8 +391,6 @@ function App() {
             </div>
           </div>
         </Slider>
-      <div style={{ width:"400px" , justifyContent: 'center', alignItems: 'center' }}>
-      <div className="midprofit">
         <div className="profit">
         <img 
               src={"/img/titleprofit.png"}></img>
@@ -400,9 +398,7 @@ function App() {
           <div className="profit1">
               <img src={"/img/profit.jpeg"}></img>
             </div>  
-      </div>
-    </div>
-        <div className="footerInfo">
+        {/* <div className="footerInfo">
           <div>상호명:(주)SB 글로벌 투자그룹 </div>
           <div>대표자:엄원택 </div>
 
@@ -410,7 +406,7 @@ function App() {
           <div>
             소재지: 고양시 대자동 업종:증권정보교육 프로그램개발 및 판매
           </div>
-        </div>
+        </div> */}
       </div>
       <h3
         style={{
