6a4ff3dd01b30e0ca1c77c06c9fb7a98313b6448
응띠쀼 <chldmsdud827@naver.com>
2023-05-12 11:19:57 +0900

    넘어가기

---
+++ src/script.js
@@ -0,0 +1,40 @@
+<div>
+    <div class="mask_privacy" style="display: block;"></div>
+        <div class="pop-layer pop-policy" style="display: block;">
+          <h1></h1>
+          <div class="contents"><div class="textarea yak_div">
+          <div id="yak_pop_1" class="yak_pop" style="">
+            <p  style="text-align:center"><span style="line-height:115%;">개인정보 제3자 제공 동의</span></p>
+            <h4><br></h4>
+            <p><b>(1) 서비스 안내 및 이용권유 등</b></p>
+            <p><br></p>
+            <p>&nbsp; ①&nbsp;제공받는 자 :&nbsp;<b>청개구리 급등주 주식종목</b></p>
+            <p>&nbsp; ② 제공목적 :&nbsp;<b>서비스&nbsp;안내&nbsp;및&nbsp;이용권유,&nbsp;사은·판촉행사&nbsp;등의&nbsp;마케팅&nbsp;활동,&nbsp;시장조사&nbsp;및&nbsp;상품·서비스&nbsp;개발연구&nbsp;등&nbsp;고객데이터&nbsp;수집&nbsp;및&nbsp;관리</b></p>
+            <p>&nbsp; ③ 수집항목&nbsp;:&nbsp;이름,&nbsp;휴대폰번호<br></p>
+            <p>&nbsp; ④ 수집 및&nbsp;이용기간&nbsp;:&nbsp;<b>문의&nbsp;종료일로&nbsp;2년까지</b></p>
+            <p><br></p>
+            <p><br></p>
+            <p>회원님은&nbsp;동의를&nbsp;거부할&nbsp;권리가&nbsp;있으며&nbsp;동의&nbsp;거부&nbsp;시에도&nbsp;서비스&nbsp;이용에&nbsp;제한이&nbsp;없습니다.&nbsp;다만&nbsp;서비스&nbsp;이용권유,&nbsp;판촉행사&nbsp;등의&nbsp;유익한&nbsp;정보를&nbsp;받으실&nbsp;수&nbsp;없습니다.&nbsp;</p>
+          </div>	<div id="yak_pop_2" style="display:none;" class="yak_pop">
+            <p  style="text-align:center"><span style="line-height:115%;">광고성 정보수신 동의</span></p>
+            <h4><br></h4>
+            <p><b>(1) 서비스 안내 및 이용권유 등</b></p>
+            <p><br></p>
+            <p>&nbsp; ①&nbsp;제공받는 자 :&nbsp;<b>청개구리 급등주 주식종목</b></p>
+            <p>&nbsp; ② 제공목적 :&nbsp;<b>서비스&nbsp;안내&nbsp;및&nbsp;이용권유,&nbsp;사은·판촉행사&nbsp;등의&nbsp;마케팅&nbsp;활동,&nbsp;시장조사&nbsp;및&nbsp;상품·서비스&nbsp;개발연구&nbsp;등&nbsp;고객데이터&nbsp;수집&nbsp;및&nbsp;관리</b></p>
+            <p>&nbsp; ③ 수집항목&nbsp;:&nbsp;이름,&nbsp;휴대폰번호<br></p>
+            <p>&nbsp; ④ 수집 및&nbsp;이용기간&nbsp;:&nbsp;<b>문의&nbsp;종료일로&nbsp;2년까지</b></p>
+            <p><br></p>
+            <p><br></p>
+            <p>회원님은&nbsp;동의를&nbsp;거부할&nbsp;권리가&nbsp;있으며&nbsp;동의&nbsp;거부&nbsp;시에도&nbsp;서비스&nbsp;이용에&nbsp;제한이&nbsp;없습니다.&nbsp;다만&nbsp;서비스&nbsp;이용권유,&nbsp;판촉행사&nbsp;등의&nbsp;유익한&nbsp;정보를&nbsp;받으실&nbsp;수&nbsp;없습니다.&nbsp;</p>
+          </div>																												</div></div>
+          <a style="float:right; color:white; cursor:pointer; margin-right: 10px;" href="javascript:privacy_pop('hide', '.pop-policy');" class="close">X</a>
+        </div>
+        
+        
+          
+          
+        <div id="mask_invest"></div><iframe id="ifr_blank" style="display:none;width:100%;height:500px;"></iframe>
+        
+      <script src="script.js"></script>
+      </div>
