6c6a5df0c5e7f73a59b03342472335c0b0df4748
응띠쀼 <chldmsdud827@naver.com>
2023-05-15 16:35:17 +0900

    인덱스

---
+++ src/index1.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index1.css';
+import App from './App2';
+import reportWebVitals from './reportWebVitals';
+
+<a href="#" onclick="openPopup(); return false;">[보기]</a>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App2 />
+    
+    </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
