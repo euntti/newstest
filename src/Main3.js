2577037aa63feff9fe184dbb2469201da64c7152
응띠쀼 <chldmsdud827@naver.com>
2023-05-19 19:00:21 +0900

    적용

--- src/Main3.js
+++ src/Main3.js
@@ -359,7 +359,7 @@ function App2() {
               </div>
               <div className="time">
               <select>
-                <option key="1" value="9">
+                <option key="9" value="9">
                   06:00-09:00
                   09:00-11:00
                   11:00-13:00
