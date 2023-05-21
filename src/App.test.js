8e98a339d4c367344a215ae917374e58533f34d6
응띠쀼 <chldmsdud827@naver.com>
2023-05-21 19:37:45 +0900

    앱투

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import App2 from './App2';
+
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('renders learn react link', () => {
+  render(<App2 />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
