df42ca1872903f440e280e58f99da1831e3030e8
응띠쀼 <chldmsdud827@naver.com>
2023-05-24 18:12:53 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./Main";
 import Main2 from "./Main2";
 import Main3 from "./Main3";
-
+import Main4 from "./Main4";
 
 const App = () => {
   return (
@@ -13,7 +13,7 @@ const App = () => {
           <Route path="/" element={<Main />}></Route>
           <Route path="/news" element={<Main2 />}></Route>
           <Route path="/test" element={<Main3 />}></Route>
-          
+          <Route path="/notice" element={<Main4 />}></Route>
 
           {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
         </Routes>
