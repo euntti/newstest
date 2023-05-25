d0caeafe0d26dac0e86b5350cdb23242e4ea295a
응띠쀼 <chldmsdud827@naver.com>
2023-05-25 17:42:30 +0900

    수정

--- src/Main5.js
+++ src/Main5.js
@@ -358,71 +358,7 @@ function App4() {
                     alt=""
                     />
                   </div>
-                    <table>
-                    <thead>
-                      <tr>
-                       <th>Date</th>
-                       <th>Username</th>
-                       <th>Telephone</th>
-                       </tr>
-                      </thead>
-                       <tbody>
-                        <tr>
-                       <div class="date">2023-05-25</div>
-                      <div class="username">김*영</div>
-                       <div class="tel">010-****-8245</div>
-                       </tr>
-                       </tbody>
-                       </table>
-                                    <li>
-                                    
-                                        <div class="date">2023-05-25</div>
-                                        <div class="username">최*필</div>
-                                        <div class="tel">010-****-8108</div>
-                                    </li>
-                                    <li>
-                                        <div class="date">2023-05-25</div>
-                                        <div class="username">이*라</div>
-                                        <div class="tel">010-****-1978</div>
-                                    </li>
-                                    <li>
-                                        <div class="date">2023-05-25</div>
-                                        <div class="username">박*희</div>
-                                        <div class="tel">010-****-9357</div>
-                                    </li>
-                                    <li>
-                                        <div class="date">2023-05-25</div>
-                                        <div class="username">김*철</div>
-                                        <div class="tel">010-****-8956</div>
-                                    </li>
-                                    <li>
-                                        <div class="date">2023-05-25</div>
-                                        <div class="username">정*구</div>
-                                        <div class="tel">010-****-5349</div>
-                                    </li>
-                                    <li>
-                                        <div class="date">2023-05-25</div>
-                                        <div class="username">이*영</div>
-                                        <div class="tel">010-****-1194</div>
-                                    </li>
-                                    <li>
-                                        <div class="date">2023-05-25</div>
-                                        <div class="username">박*국</div>
-                                        <div class="tel">010-****-7191</div>
-                                    </li>
-                                    <li>
-                                        <div class="date">2023-05-25</div>
-                                        <div class="username">이*란</div>
-                                        <div class="tel">010-****-8546</div>
-                                    </li>
-                                    <li>
-                                        <div class="date">2023-05-25</div>
-                                        <div class="username">지*권</div>
-                                        <div class="tel">010-****-1751</div>
-                                    </li>
-
-                                
-                                <table>
+                  <table>
   <thead>
     <tr>
       <th>Date</th>
@@ -440,6 +376,41 @@ function App4() {
       <td>2023-05-25</td>
       <td>최*필</td>
       <td>010-****-8108</td>
+    </tr>
+    <tr>
+      <td>2023-05-25</td>
+      <td>김*우</td>
+      <td>010-****-3040</td>
+    </tr>
+    <tr>
+      <td>2023-05-25</td>
+      <td>이*영</td>
+      <td>010-****-5504</td>
+    </tr>
+    <tr>
+      <td>2023-05-25</td>
+      <td>지*우</td>
+      <td>010-****-9913</td>
+    </tr>
+    <tr>
+      <td>2023-05-25</td>
+      <td>이*현</td>
+      <td>010-****-3342</td>
+    </tr>
+    <tr>
+      <td>2023-05-25</td>
+      <td>구*현</td>
+      <td>010-****-7050</td>
+    </tr>
+    <tr>
+      <td>2023-05-25</td>
+      <td>이*수</td>
+      <td>010-****-6703</td>
+    </tr>
+    <tr>
+      <td>2023-05-25</td>
+      <td>심*우</td>
+      <td>010-****-4022</td>
     </tr>
    
   </tbody>
