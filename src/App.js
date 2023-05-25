2390695b8e198b39d02b96d6a113b1b162019133
응띠쀼 <chldmsdud827@naver.com>
2023-05-25 11:47:05 +0900

    인식이안됨

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ const App = () => {
           <Route path="/news" element={<Main2 />}></Route>
           <Route path="/test" element={<Main3 />}></Route>
           <Route path="/notice" element={<Main4 />}></Route>
-          <Route path="/sbgallery" element={<Main5 />}></Route>
+          <Route path="/gallery" element={<Main5 />}></Route>
+
+
           {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
         </Routes>
       </BrowserRouter>
