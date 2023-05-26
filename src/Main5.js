280d9ea98b87cf09bb5819c05d9c847aa395f6e2
응띠쀼 <chldmsdud827@naver.com>
2023-05-26 10:41:09 +0900

    날짜업뎃

--- src/Main5.js
+++ src/Main5.js
@@ -26,7 +26,37 @@ const customStyles = {
 };
 
 function App4() {
+  function getCurrentDate() {
+    var currentDate = new Date();
+    var year = currentDate.getFullYear();
+    var month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    var day = String(currentDate.getDate()).padStart(2, '0');
+    var formattedDate = year + '-' + month + '-' + day;
+    return formattedDate;
+  }
 
+  function updateTableDate() {
+    var dateCell1 = document.getElementById('date-cell-1');
+    var dateCell2 = document.getElementById('date-cell-2');
+    var dateCell3 = document.getElementById('date-cell-3');
+    var dateCell4 = document.getElementById('date-cell-4');
+    var dateCell5 = document.getElementById('date-cell-5');
+    var dateCell6 = document.getElementById('date-cell-6');
+    var dateCell7 = document.getElementById('date-cell-7');
+    // 필요한 만큼의 dateCell 변수를 추가로 선언하여 날짜 업데이트
+
+    dateCell1.textContent = getCurrentDate();
+    dateCell2.textContent = getCurrentDate();
+    dateCell3.textContent = getCurrentDate();
+    dateCell4.textContent = getCurrentDate();
+    dateCell5.textContent = getCurrentDate();
+    dateCell6.textContent = getCurrentDate();
+    dateCell7.textContent = getCurrentDate();
+    // dateCell 변수에 따라 업데이트
+
+  }
+
+  updateTableDate();
 
 
   const [users, setUsers] = useState([]);
@@ -362,35 +392,29 @@ function App4() {
                     />
                   </div>
                   <table className="tablelive" >
-                  
-  <thead>
-    <tr>
-      <th>Date</th>
-      <th>Username</th>
-      <th>Telephone</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td id="date-cell-1">2023-05-26</td>
-      <td>김*영</td>
-      <td>010-****-8245</td>
-    </tr>
-    <tr>
-      <td id="date-cell-2">2023-05-26</td>
-      <td>최*필</td>
-      <td>010-****-8108</td>
-    </tr>
-    <tr>
-      <td id="date-cell-3">2023-05-26</td>
-      <td>김*우</td>
-      <td>010-****-3040</td>
-    </tr>
-    <tr>
-      <td id="date-cell-4">2023-05-26</td>
-      <td>이*영</td>
-      <td>010-****-5504</td>
-    </tr>
+                   <thead>
+                    </thead>
+                  <tbody>
+                    <tr>
+                    <td id="date-cell-1">2023-05-26</td>
+                    <td>김*영</td>
+                    <td>010-****-8245</td>
+                    </tr>
+                    <tr>
+                    <td id="date-cell-2">2023-05-26</td>
+                    <td>최*필</td>
+                    <td>010-****-8108</td>
+                    </tr>
+                    <tr>
+                    <td id="date-cell-3">2023-05-26</td>
+                    <td>김*우</td>
+                    <td>010-****-3040</td>
+                    </tr>
+                    <tr>
+                    <td id="date-cell-4">2023-05-26</td>
+                    <td>이*영</td>
+                    <td>010-****-5504</td>
+                    </tr>
     <tr>
       <td id="date-cell-5">2023-05-26</td>
       <td>지*우</td>
