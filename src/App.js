40cdb6926759ab95e2e99aa86883d69a441160b0
응띠쀼 <chldmsdud827@naver.com>
2023-05-30 14:29:20 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -10,13 +10,16 @@ const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
+      <Switch>
+        <Route exact path="/index.html" component={Main5} /> {/* index.html 경로에 Main5 컴포넌트 렌더링 */}
+        {/* 다른 페이지에 대한 라우팅 설정 */}
+      </Switch>
         <Routes>
           <Route path="/" element={<Main />}></Route>
           <Route path="/news" element={<Main2 />}></Route>
           <Route path="/test" element={<Main3 />}></Route>
           <Route path="/notice" element={<Main4 />}></Route>
-          <Route path="/gallery" element={<Main5 />}></Route>
-
+          <Route path="/gallery" exact ="/index.html" component={Main5} element={<Main5 />}></Route>
 
           {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
         </Routes>
