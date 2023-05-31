96a429ab150772fcdbdeed30d14eac11ba29eec0
응띠쀼 <chldmsdud827@naver.com>
2023-05-31 15:17:41 +0900

    수익률

--- src/Main.js
+++ src/Main.js
@@ -466,9 +466,9 @@ function App() {
             <img style={{ width: "390px" }} src={"/img/mtitleprofit.png"}></img>
           )}
         </div> */}
-        <div className="profit1">
+        {/* <div className="profit1">
           <img src={"/img/profit.jpeg"}></img>
-        </div>
+        </div> */}
         <div className="footerInfo">
           <div>상호명:(주)SB 글로벌 투자그룹 </div>
           <div>대표자:엄원택 </div>
