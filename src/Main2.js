ecd1b02b02affc6276dc583c4dd6edd9f0b3993d
응띠쀼 <chldmsdud827@naver.com>
2023-05-31 18:24:02 +0900

    벌루

--- src/Main2.js
+++ src/Main2.js
@@ -163,11 +163,7 @@ function Main2() {
           <div className="content-wrap">
             <div className="content">
                <p class="imgbox banner">
-                <img
-                  style={{ width: 620, height: "auto" }}
-                  src={}
-                  alt=""
-                />
+               
               </p> 
               <p>
                 <b>"주식투자&가상화폐로 매월 3천이상의 꾸준한 수익을 내고 있어요.</b>
