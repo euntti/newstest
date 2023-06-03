55d7eb5154469fea5b98d8c791a79d8650440e60
응띠쀼 <chldmsdud827@naver.com>
2023-06-03 20:19:04 +0900

    수정

--- src/Main7.js
+++ src/Main7.js
@@ -457,7 +457,7 @@ function App6() {
             <div className="btnArea">
               <button onClick={(e) => submitEvent(e)}>
                 {isBrowser ? (
-                  <img src={"/img/btn.gif"}></img>
+                  <img src={"/img/mainbtn.png"}></img>
                 ) : (
                   <img style={{ width: "390px" }} src={"/img/btn.gif"}></img>
                 )}
