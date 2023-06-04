15a5a36b066f1740b78730f505b32b8b82413f47
응띠쀼 <chldmsdud827@naver.com>
2023-06-04 12:04:30 +0900

    폴더추가

---
+++ src/LazyImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LazyImage = () => (
+  <img
+    src={"/img/sbstock.png"}
+    style={{ width: 1268, height: "auto", marginLeft: 20 }}
+    alt="logo"
+  />
+);
+
+export default LazyImage;
