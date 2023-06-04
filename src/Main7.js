e3ad9bef8601ed2b67a2711cc81bd7392be057f5
응띠쀼 <chldmsdud827@naver.com>
2023-06-04 23:57:48 +0900

    수정

--- src/Main7.js
+++ src/Main7.js
@@ -44,10 +44,10 @@ function App6() {
     } else if (formattedNumber.length >= 6) {
       formattedNumber = formattedNumber.replace(/(\d{3})(\d{3})(\d{0,4})/, "$1-$2-$3"); // 첫 번째와 두 번째 하이픈 추가
     }
-    setPhone1(formattedNumber);
+    setPhone17(formattedNumber);
 
     const inputValue = e.target.value.replace(/[^0-9]/g, ""); // 숫자 이외의 문자 제거
-    setPhone1(inputValue);
+    setPhone17(inputValue);
   };
 
   
