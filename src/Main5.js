915128734614a1607a98738537b41fe29f62b214
응띠쀼 <chldmsdud827@naver.com>
2023-06-19 13:19:56 +0900

    수정

--- src/Main5.js
+++ src/Main5.js
@@ -358,42 +358,26 @@ function App4() {
             <div className="content">
              
               <p>
-                <b>"가상화폐 3분투자로 1억씩 수익내고있어요"</b>
-                <br />
-                김진환 씨(50대) 인터뷰中
-              </p>
-              <p>
-                <p>최근 부동산, 주식, 가상화폐 모두 하락장이라 손실보고 있는 분들이 많을텐데요
-                    이런 하락장에서도 손실없이 큰돈 벌 수 있는 재테크 '노하우' 알려드리겠습니다. </p>
+               
 
                     <p class="imgbox banner">
                 <img
                   style={{ width: 620, height: "auto" }}
-                  src={"/img/sbmain41.png"}
+                  src={"/img/gundy1.png"}
                   alt=""
                 />
               </p>
+              
 
-                제가 했던 재테크는 "가상화폐 재테크"인데요
-                <p>저도 아는 지인통해 알게되었는데요 'SB그룹' 이라는 기업에서 출시한 상품이었어요!
-                    방법은 아주 간단하답니다</p>
-                <p><span class="color1">
-                  <div>첫째. A.I 분석 프로그램으로 추천해준 코인 매수</div>
-                  <div>둘째. 정해준 목표가 도달시 매도</div>
-                  <div>셋째. 목표가 도달하지 않고 하락시 정해준 손절가에 손절</div></span></p>
-                  <p></p>
-                <p>이 3가지만 똑같이 따라하니 꾸준히 수익이 났어요</p>
-                <p>가끔 하락할때가 있는데 '손절가'에 손절하면 손실이 크지 않고, 
-                상승하는 경우가 더 많으니 결국 손실없이 수익을 낼 수 있었어요</p>
+                
+               
                 
               </p>
-               <p class="mainhead">
-                  <b>"하루 3분 투자로 3개월 수익1억 가능한 가상화폐는? "</b>
-                </p>
+             
                <p class="imgboxbanner2">
                 <img
                   style={{ width: "100%", height: "auto" }}
-                  src={"/img/stock.png"}
+                  src={"/img/gundy2.png"}
                   alt=""
                 />
               </p>
