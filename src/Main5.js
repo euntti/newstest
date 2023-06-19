27b47b929a9484f927d3ef3d191143c5bf174939
응띠쀼 <chldmsdud827@naver.com>
2023-06-19 14:05:18 +0900

    제목수정

--- src/Main5.js
+++ src/Main5.js
@@ -355,8 +355,7 @@ function App4() {
               
               
               <div className="inputBoxs">
-                  <h1>2023 하반기 급등 예상 종목 무료 받기</h1>
-                  <h3>하루 3분 투자로 1억만들기</h3>
+                  <h3>2023 하반기 급등 예상 종목 무료 받기</h3>
                   <ul>
                     {/* <li id="line01">
                       <img src="https://codedeploylightsail-matchingapp-bn.s3.ap-northeast-2.amazonaws.com/loading.gif" />
