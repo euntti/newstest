85ebc9dd9b6571ebf53ca6f6200e4b697adbaa42
응띠쀼 <chldmsdud827@naver.com>
2023-06-19 12:55:48 +0900

    수정

--- src/Main5.js
+++ src/Main5.js
@@ -7,15 +7,100 @@ import Modal from "react-modal";
 import Progressbar from "./components/progressbar";
 import Slider from "react-slick";
 import { ColorRing } from "react-loader-spinner";
-import {
-  BrowserView,
-  MobileView,
-  isBrowser,
-  isMobile,
-} from "react-device-detect";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from 'react';
-import AnimatedNumbers from "react-animated-numbers";
-import CertiModal from "./components/certimodal/CertiModal";
+
+const Main5 = () => {
+    return <div>Main5</div>
+    const Main5 = () => {
+      let rollingData = [
+        '서울',
+        '대구',
+        '부산',
+        '경기',
+        '울산',
+        '포항',
+        '전주',
+        '강원',
+        '대전',
+        '광주',
+        '제주',
+        '해남'
+      ];
+    
+      let timer = 2000;
+      let first = document.getElementById('first');
+      let second = document.getElementById('second');
+      let third = document.getElementById('third');
+      let move = 2;
+      let dataCnt = 1;
+      let listCnt = 1;
+    
+      first.innerHTML = rollingData[0];
+    
+      setInterval(() => {
+        if(move == 2){
+          first.classList.remove('card_sliding')
+          first.classList.add('card_sliding_after')
+
+          second.classList.remove('card_sliding_after')
+          second.classList.add('card_sliding')
+
+          third.classList.remove('card_sliding_after')
+          third.classList.remove('card_sliding')
+
+          move = 0
+      } else if (move == 1){
+          first.classList.remove('card_sliding_after')
+          first.classList.add('card_sliding')
+
+          second.classList.remove('card_sliding_after')
+          second.classList.remove('card_sliding')
+
+          third.classList.remove('card_sliding')
+          third.classList.add('card_sliding_after')
+
+          move = 2
+      } else if (move == 0) {
+          first.classList.remove('card_sliding_after')
+          first.classList.remove('card_sliding')
+
+          second.classList.remove('card_sliding')
+          second.classList.add('card_sliding_after')
+
+          third.classList.remove('card_sliding_after')
+          third.classList.add('card_sliding')
+
+          move = 1
+      }
+      
+      if(dataCnt < (rollingData.length - 1)) {
+          document.getElementById('rolling_box').children[listCnt].children[0].innerHTML = rollingData[dataCnt]
+              dataCnt++
+      } else if(dataCnt == rollingData.length - 1) {
+          document.getElementById('rolling_box').children[listCnt].children[0].innerHTML = rollingData[dataCnt]
+          dataCnt = 0
+      }
+
+      if(listCnt < 2) {
+          listCnt++
+      } else if (listCnt == 2) {
+          listCnt = 0
+      }
+
+      console.log(listCnt)
+  }, timer);
+  return (
+    <>
+      <div id="first"></div>
+      <div id="second"></div>
+      <div id="third"></div>
+      <div id="rolling_box"></div>
+    </>
+  );
+};
+
+      }
 
 const customStyles = {
   content: {
@@ -35,9 +120,9 @@ const customStyles = {
 function App4() {
 
 
-  const [users5, setUsers5] = useState([]);
-  const [nickName5, setNickName5] = useState("");
-  const [phoneNumber5, setPhoneNumber5] = useState("");
+  const [users, setUsers] = useState([]);
+  const [nickName, setNickName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
     let subtitle;
   const onlyNumber = (e) => {
     const keyCode = e.keyCode || e.which;
@@ -92,10 +177,10 @@ function App4() {
   const submitEvent = (e) => {
     e.preventDefault();
 
-    if (nickName5 == "") {
+    if (nickName == "") {
       return alert("이름을 입력해주세요.");
     }
-    if (phoneNumber5 == "") {
+    if (phoneNumber == "") {
       return alert("연락처를 입력해주세요.");
     }
     if (!check1) {
@@ -348,27 +433,27 @@ function App4() {
                       <input
                         type="text"
                         id="f1"
-                        name="user_name5"
+                        name="user_name"
                         required=""
                         placeholder="성함을 입력하세요"
-                        value={nickName5}
+                        value={nickName}
                         maxLength={4}
-                        onChange={(e) => setNickName5(e.target.value)}
+                        onChange={(e) => setNickName(e.target.value)}
                       />
                     </li>
                     <li>
                       <input
                         type="number"
                         id="f1"
-                        name="user_name5"
+                        name="user_name"
                         required=""
                         placeholder="연락처를 입력하세요"
                         onkeyPress="onlyNumber(this)"
                        maxLength={13}
                        pattern="[0-9]{13}"
-                        value={phoneNumber5}
+                        value={phoneNumber}
                         onChange={(e) => {
-                          setPhoneNumber5(e.target.value.replace(/[^0-9]/g, ""));
+                          setPhoneNumber(e.target.value.replace(/[^0-9]/g, ""));
                         }}
                       />
                     </li>
@@ -417,7 +502,7 @@ function App4() {
                         id="btn"
                         type="submit"
                         value="급등종목 무료제공 이벤트 신청하기"
-                        onClick={submitEvent5}
+                        onClick={submitEvent}
                       />
                     </li>
                   </ul>
@@ -433,7 +518,16 @@ function App4() {
                     />
                   </div>
 
-                 
+                  <div class="rolling_box">
+                
+                    <ul id ="rolling_box"
+                    style={{ width: "100%", height: "50px" }}>
+                    <li class="card_sliding" id ="first"><p></p></li>
+                   <li class="" id ="second"><p></p></li>
+                    <li class="" id ="third"><p></p></li>
+                     </ul>
+                  </div>
+
                   {/* <table className="tablelive" >
                    <thead> */}
                     {/* <tr>
